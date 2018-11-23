d41a62c8e7b5e5b799b97be4df301ef8ee11f1e7
snbaredd <snbaredd@DIN73003132.corp.capgemini.com>
2018-11-23 13:57:49 +0530

    first commit

---
+++ Day1-BDD/src/main/java/com/capgemini/bdd/BddDemo.java
@@ -0,0 +1,16 @@
+package com.capgemini.bdd;
+
+public class BddDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Helloo !!");
+		int add = sum(4,5);
+
+	}
+
+	private static int sum(int i, int j) {
+		return i+j;
+	}
+
+}
